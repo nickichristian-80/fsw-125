74c0e7e9de207bb79e79c3b6ffbf1a26575a6d9d
nickichristian-80 <nicki.christian@bryanuniversity.edu>
2021-02-02 08:40:44 -0600

    commit

--- wk1/math/app.js
+++ wk1/math/app.js
@@ -1,4 +1,9 @@
-const {sum, subtract} = require("./math.js")
+const {sum, subtract,multiply, divide} = require("./math.js")
 
-console.log(sum, subtract(10, 20))
+console.log(sum(10, 20))
 
+console.log(subtract(20, 30))
+
+console.log(multiply(20, 5))
+
+console.log(divide(72, 4))
